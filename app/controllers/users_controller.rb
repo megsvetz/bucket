a2ca6392e8580f7b41901cfad9bd1a5844ef016e
Meg Svetz <megsvetz@gmail.com>
2015-06-29 16:09:30 -0600

    model views added paperclip params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 	before_action :authenticate_user!
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  
 	def index
     @users = User.all
   end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ActiveRecord::Base
 	belongs_to :user
 	has_many :goals
+  has_attached_file :cat_pic, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :cat_pic, :content_type => /\Aimage\/.*\Z/
+
 end
