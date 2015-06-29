c126b44d443eca0a44c3767ace63ae93d15a42c6
Meg Svetz <megsvetz@gmail.com>
2015-06-29 16:37:35 -0600

    home page view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: params[:id])
+    # @user = User.find_by(id: params[:id])
+    @categories = Category.all
   end
 end
