b5f42bfc16d66705524c530f311a597489b43d5c
Meg Svetz <megsvetz@gmail.com>
2015-06-29 16:23:39 -0600

    added home page equation

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   resources :goals 
 
   root 'pages#home'
-  get 'users/index'
+  get 'users/index' => 'users#index', as: :users
+  # get ':id' => "users#show"
+  get 'users/:id' => 'users#show', as: :user_show
+
 end
+
