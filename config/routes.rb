69fa5466539a07010c7bcb1783dfe1c24541531d
Meg Svetz <megsvetz@gmail.com>
2015-06-29 17:35:36 -0600

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
+  devise_for :users, controllers: {registrations: "registrations"}
+  
   get 'users/index' => 'users#index', as: :users
 
   # get ':id' => "users#show"
   get 'users/:id' => 'users#show', as: :user_show
 
-  resources :categories do 
-    resources :goals
-  end 
-  devise_for :users, controllers: {registrations: "registrations"}
-
+  resources :categories  
+  resources :goals
 end
