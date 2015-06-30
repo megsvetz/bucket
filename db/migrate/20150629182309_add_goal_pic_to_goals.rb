15cb6cda516ed8593aa0b396f31de8e5f8fe3667
Meg Svetz <megsvetz@gmail.com>
2015-06-30 09:37:43 -0600

    changed view/user/show page

--- db/migrate/20150629182309_add_goal_pic_to_goals.rb
+++ db/migrate/20150629182309_add_goal_pic_to_goals.rb
@@ -7,3 +7,16 @@ class AddGoalPicToGoals < ActiveRecord::Migration
     remove_attachment :goals, :goal_pic
   end
 end
+
+
+class Items < ActiveRecord::Migration
+  def self.up
+    change_table :items do |t|
+      t.attachment :item_pic
+    end
+  end
+
+  def self.down
+    remove_attachment :items, :item_pic
+  end
+end
