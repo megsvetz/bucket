0351d54016faaf7b349fa426fc463aeb6c86f038
stephyc93 <stephyc93@gmail.com>
2015-06-29 18:13:01 -0600

    added user index view and fixed route to working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
 	before_action :authenticate_user!
-  
+
 	def index
     @users = User.all
+		@categories = Category.order(created_at: :desc)
   end
 
   def new
