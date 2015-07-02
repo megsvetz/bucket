00dfba6ad2191a8aa496330c58b9213118c0e016
Doyi Lee <doyilee@hotmail.com>
2015-07-02 15:15:25 -0600

    finished pages controller specs with rspec

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,16 +4,5 @@ class PagesController < ApplicationController
 	  # @name = User.find_by(id: goal.user_id)
 	  @goals = Goal.top_six
 	  @users = User.all
-
-	  config = {
-	    consumer_key: ENV['key'],
-	    consumer_secret: ENV['secret'],
-	    access_token: ENV['token'],
-	    access_token_secret: ENV['token_secret']
-	  }
-	  client = Twitter::REST::Client.new(config)
-	  # @tweets = client.get_all_tweets('doyilee')
-	  user = client.user('doyilee')
-
 	end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	let(:user) {User.create(username: "girl", email: "girl@example.com", password: "password")}
+
+  describe "GET #home" do
+   	it "returns http success" do
+	    get :home
+	    expect(response).to have_http_status(:success)
+   	end
+
+   	it "returns all users"do 
+   		user
+   		expect(User.first.username).to eq("girl")
+   	end
+  end
 end
