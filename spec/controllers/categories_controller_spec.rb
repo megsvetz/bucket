3cf44688a336bb5c3b82e6c41bb8b50b365ba94c
Doyi Lee <doyilee@hotmail.com>
2015-07-02 12:08:06 -0600

    added controller specs

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,1 +1,14 @@
-c
+require 'rails_helper'
+
+RSpec.describe CategoriesController, type: :controller do
+	context "ordering" do
+    let(:category) {Category.create(name: "Summer")}
+  	end
+	  describe "#index" do
+	    it "returns recently updated bucket" do
+	      put :update, id: category.id, category: {name: "Fall"}
+			  expect(Category.first.name).to eq("Fall")
+			end
+	  end
+	end
+end
--- spec/controllers/omniauth_callbacks_controller.rb
+++ spec/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe ApplicationController, type: :controller do
+RSpec.describe OmniauthCallbacksController, type: :controller do
   pending "add some examples to (or delete) #{__FILE__}"
 end
