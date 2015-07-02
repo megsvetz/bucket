f4cfd8ae0105b89b049c29e9692a2fb839980b9e
Doyi Lee <doyilee@hotmail.com>
2015-07-02 14:35:02 -0600

    removed authentication from application controller

--- spec/controllers/goals_controller_spec.rb
+++ spec/controllers/goals_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe GoalsController, type: :controller do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "#goal index" do
+  	before :each do
+  	end
+
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns all goals of category" do 
+    	
+    end
+  end
 end
