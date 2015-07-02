f3bdb0a64d5820ac0c92a9bc396544f37ee56d92
stephyc93 <stephyc93@gmail.com>
2015-07-02 12:45:53 -0600

    tested for identity model associations complete

--- spec/models/identity_spec.rb
+++ spec/models/identity_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Identity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "associations" do
+    it { should belong_to ( :user ) }
+    it { should validate_presence_of ( :uid ) }
+    it { should validate_uniqueness_of ( :uid ) }
+  end
+  #
+  # context "class methods" do
+  #   describe ".find_for_twitter_oauth" do
+  #   end
+  # end
+
 end
